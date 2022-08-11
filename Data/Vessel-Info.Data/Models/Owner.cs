2f9a331ddfd30edfbf6e40c8677d36c8252cabd8
george-pench <penchev.georgii@gmail.com>
2022-08-11 23:38:27 +0300

    Add additional vessel's properties and an Owner entity

---
+++ Data/Vessel-Info.Data/Models/Owner.cs
@@ -0,0 +1,19 @@
+﻿namespace Vessel_Info.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ushort Founded { get; set; }
+
+        public string Website { get; set; }
+
+        public IEnumerable<Vessel> Vessels { get; set; } = new List<Vessel>();
+    }
+}
--- Data/Vessel-Info.Data/Models/Vessel.cs
+++ Data/Vessel-Info.Data/Models/Vessel.cs
@@ -8,14 +8,24 @@
 
         public string Name { get; set; }
 
+        public string ExName { get; set; }
+
         public int IMO { get; set; }
 
         public string CallSign { get; set; }
 
         public DateTime Built { get; set; }
 
+        public int SummerDwt { get; set; }
+
+        public string HullType { get; set; }
+
         public int TypeId { get; set; }
 
         public Type Type { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public Owner Owner { get; set; }
     }
 }
