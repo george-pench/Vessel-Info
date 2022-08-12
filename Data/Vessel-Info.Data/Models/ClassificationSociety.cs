b7f4d836d2eaf0b1759d1d9e37088b62ecd22ae0
george-pench <penchev.georgii@gmail.com>
2022-08-12 15:40:16 +0300

    Add ClassificationSociety and Registration entities

---
+++ Data/Vessel-Info.Data/Models/ClassificationSociety.cs
@@ -0,0 +1,21 @@
+﻿namespace Vessel_Info.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    public class ClassificationSociety
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Abbreviation { get; set; }
+
+        public DateTime Founded { get; set; }
+
+        public IEnumerable<Vessel> Vessels { get; set; } = new List<Vessel>();
+    }
+}
---
+++ Data/Vessel-Info.Data/Models/Registration.cs
@@ -0,0 +1,18 @@
+﻿namespace Vessel_Info.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    public class Registration
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Flag { get; set; }
+
+        [Required]
+        public string RegistryPort { get; set; }
+
+        public IEnumerable<Vessel> Vessels { get; set; } = new List<Vessel>();
+    }
+}
