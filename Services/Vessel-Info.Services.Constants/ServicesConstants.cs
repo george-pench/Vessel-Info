ace1fc4766feab3a6a715e9a65bcb752befb1d7a
george-pench <penchev.georgii@gmail.com>
2022-08-28 01:39:26 +0300

    Add services constants into a separate project.

---
+++ Services/Vessel-Info.Services.Constants/ServicesConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace Vessel_Info.Services.Constants
+{
+    public class ServicesConstants
+    {
+        public const char START_LETTER = 'X';
+        public const char END_LETTER = 'Y';
+
+        public const string BASE_URL_BY_CHARACTER = "https://www.q88.com/ships.aspx?letter={0}&v=list";
+        public const string BASE_URL_BY_ID = "https://www.q88.com/ships.aspx?letter={id}&v=list";
+        public const string ERROR_MESSAGE = "No vessels starting with";
+
+        public const string VESSEL_NAME_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td > a";
+        public const string IMO_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(2)";
+        public const string BUILT_DATA_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(3)";
+        public const string DWT_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(4)";
+        public const string LOA_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(5)";
+        public const string CUBIC_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(6)";
+        public const string BEAM_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(7)";
+        public const string DRAFT_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(8)";
+        public const string HULL_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(9)";
+        public const string CALL_SIGN_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(10)";
+        public const string OWNER_SELECTOR = "#pnlQuestionnaires > table.main > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(6) > td:nth-child(5)";
+        public const string TYPE_SELECTOR = "#pnlQuestionnaires > table.main > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(5) > td:nth-child(2)";
+        public const string GUID_SELECTOR = "#ctl00_cphMiddle_ctl00_modView_dgVessel > tbody > tr > td:nth-child(1)";
+        public const string FLAG_SELECTOR = "#pnlQuestionnaires > table.main > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(2)";
+        public const string PORT_SELECTOR = "#pnlQuestionnaires > table.main > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(4) > td:nth-child(2)";
+        public const string CLASS_SOCIETY_SELECTOR = "#pnlQuestionnaires > table.main > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(7) > td:nth-child(2)";
+
+        public const int SKIPNUMBER_SELECTOR = 1;
+    }
+}
---
+++ Services/Vessel-Info.Services.Models/Q88ListingServiceModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Vessel_Info.Services.Models
+{
+    using System.Collections.Generic;
+
+    public class Q88ListingServiceModel
+    {
+        public List<string> Name { get; set; } = new List<string>();
+
+        public List<string> Imo { get; set; } = new List<string>();
+
+        public List<string> Built { get; set; } = new List<string>();
+
+        public List<string> SummerDwt { get; set; } = new List<string>();
+
+        public List<string> Loa { get; set; } = new List<string>();
+
+        public List<string> Cubic { get; set; } = new List<string>();
+
+        public List<string> Beam { get; set; } = new List<string>();
+
+        public List<string> Draft { get; set; } = new List<string>();
+
+        public List<string> HullType { get; set; } = new List<string>();
+
+        public List<string> CallSign { get; set; } = new List<string>();
+    }
+}
