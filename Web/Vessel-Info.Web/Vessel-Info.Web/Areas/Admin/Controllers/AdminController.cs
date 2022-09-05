65444fd02f9423d45a5d9fdfebc7e56a683ccb16
george-pench <penchev.georgii@gmail.com>
2022-09-05 22:58:19 +0300

    Add Admin, Vessel Controllers and All.cshtml

---
+++ Web/Vessel-Info.Web/Vessel-Info.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿namespace Vessel_Info.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static Constants.WebConstants;
+
+    [Area(AreaName)]
+    [Authorize(Roles = AdministratorRoleName)]
+    public abstract class AdminController : Controller { }
+}
---
+++ Web/Vessel-Info.Web/Vessel-Info.Web/Areas/Admin/Controllers/VesselsController.cs
@@ -0,0 +1,41 @@
+﻿namespace Vessel_Info.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Vessel_Info.Services.Vessels;
+
+    public class VesselsController : AdminController
+    {
+        private readonly IVesselService vessels;
+
+        public VesselsController(IVesselService vessels)
+        {
+            this.vessels = vessels;
+        }
+
+        // [HttpGet("Admin/Vessels/All")]
+        public IActionResult All()
+        {
+            var vessels = this.vessels.All();
+
+            return this.View(vessels);
+        }
+
+        // [HttpGet("Admin/Vessels/Details")]
+        public IActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var vessel = this.vessels.Details(id);
+
+            if (vessel == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(vessel);
+        }
+    }
+}
