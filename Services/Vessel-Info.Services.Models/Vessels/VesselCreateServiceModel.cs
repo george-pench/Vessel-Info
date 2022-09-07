703b3882ee98a20a9d8558a66b5d90ec2a4879f4
george-pench <penchev.georgii@gmail.com>
2022-09-08 01:11:53 +0300

    Add VesselCreateServiceModel and Create.cshtml

---
+++ Services/Vessel-Info.Services.Models/Vessels/VesselCreateServiceModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Vessel_Info.Services.Models.Vessels
+{
+    public class VesselCreateServiceModel
+    {
+        public int TypeId { get; set; }
+
+        public int RegistrationId { get; set; }
+
+        public int ClassificationSocietyId { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public VesselAllServiceModel Vessel { get; set; }
+    }
+}
