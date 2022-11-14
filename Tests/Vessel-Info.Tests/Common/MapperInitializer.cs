cc46c17a7f9788398fdc3e968ce9699e90d624b3
george-pench <penchev.georgii@gmail.com>
2022-11-14 23:09:16 +0200

    Configure unit testing to the project. Add ClassificationSocietyServiceTest.

---
+++ Tests/Vessel-Info.Tests/Common/MapperInitializer.cs
@@ -0,0 +1,24 @@
+﻿namespace Vessel_Info.Tests.Common
+{
+    using System.Reflection;
+    using Vessel_Info.Data.Models;
+    using Vessel_Info.Services.Mapping;
+    using Vessel_Info.Services.Models.ClassSocieties;
+
+    public static class MapperInitializer
+    {
+        private static bool initialized = false;
+
+        public static void Initialize()
+        {
+            if (!initialized)
+            {
+                AutoMapperConfig.RegisterMappings(
+                    typeof(ClassSocietyAllServiceModel).GetTypeInfo().Assembly,
+                    typeof(ClassificationSociety).GetTypeInfo().Assembly);
+
+                initialized = true;
+            }
+        }
+    }
+}
---
+++ Tests/Vessel-Info.Tests/Services/ClassificationSocietyServiceTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Vessel_Info.Tests.Services
+{
+    using Data;
+    using FluentAssertions;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using Vessel_Info.Data.Models;
+    using Vessel_Info.Services.Vessels;
+    using Vessel_Info.Tests.Common;
+    using Xunit;
+
+    public class ClassificationSocietyServiceTest
+    {
+        // TODO: AAA patern
+
+        public ClassificationSocietyServiceTest()
+        {
+            MapperInitializer.Initialize();
+        }
+
+        [Fact]
+        public void GetAllBySearchTermShouldReturnCorrectResultWithWhereAndOrder()
+        {
+            // Arrange
+            var dbOptions = new DbContextOptionsBuilder<VesselInfoDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var db = new VesselInfoDbContext(dbOptions);
+
+            var firstClass = new ClassificationSociety { Id = 1, FullName = "First" };
+            var secondClass = new ClassificationSociety { Id = 2, FullName = "Second" };
+            var thirdClass = new ClassificationSociety { Id = 3, FullName = "Third" };
+
+            db.AddRange(firstClass, secondClass, thirdClass);
+            db.SaveChanges();
+
+            var classSocietyService = new ClassificationSocietyService(db);
+
+            // Act
+            var result = classSocietyService.GetAllBySearchTerm("Fi");
+
+            // Assert
+            result
+                .Should()
+                .Match(r => r.ElementAt(0).Id == 1)
+                .And
+                .HaveCount(1);
+        }
+    }
+}
