93f02966f325cf28c8af0cea8a6c446b9e977cc3
george-pench <penchev.georgii@gmail.com>
2022-12-15 22:29:20 +0200

    Add RegistrationsController tests

---
+++ Tests/Vessel-Info.Tests/Web/Controllers/RegistrationsControllerTest.cs
@@ -0,0 +1,89 @@
+﻿namespace Vessel_Info.Tests.Web.Controllers
+{
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Vessel_Info.Services.Models.Registrations;
+    using Vessel_Info.Services.Vessels;
+    using Vessel_Info.Web.Controllers;
+    using Xunit;
+
+    public class RegistrationsControllerTest
+    {
+        private const int registrationId = 1;
+        private const string registrationFlagName = "SomeRegistrationFlagName";
+
+        [Fact]
+        public async Task AllShouldReturnNotFoundWithInvalidId()
+        {
+            // Arrange
+            var controller = new RegistrationsController(null);
+
+            // Act
+            var result = await controller.All(-1);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public void DetailsShouldBeForAuthorizedUsersOnly()
+        {
+            // Arrange
+            var method = typeof(RegistrationsController).GetMethod(nameof(RegistrationsController.Details));
+
+            // Act
+            var attributes = method.GetCustomAttributes(true);
+
+            // Assert
+            attributes
+                .Should()
+                .Match(attr => attr.Any(a => a.GetType() == typeof(AuthorizeAttribute)));
+        }
+
+        [Fact]
+        public async Task DetailsShouldReturnNotFoundWithInvalidRegistrationId()
+        {
+            // Arrange
+            var controller = new RegistrationsController(null);
+
+            // Act
+            var result = await controller.Details(null);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task DetailsShouldReturnViewWithCorrectModelAndValidRegistration()
+        {
+            // Arrange
+            var registrationService = new Mock<IRegistrationService>();
+
+            registrationService
+               .Setup(cs => cs.DetailsAsync(It.IsAny<int>()))
+               .ReturnsAsync(new RegistrationBaseServiceModel { Id = registrationId, Flag = registrationFlagName });
+
+            var controller = new RegistrationsController(registrationService.Object);
+
+            // Act
+            var result = await controller.Details(registrationId);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<ViewResult>()
+                .Subject
+                .Model
+                .Should()
+                .Match(m => m.As<RegistrationBaseServiceModel>().Flag == registrationFlagName);
+        }
+    }
+}
